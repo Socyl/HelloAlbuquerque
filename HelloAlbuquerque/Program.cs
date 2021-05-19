c03fa87bf6f48a16b4302a96c1ed454558434457
DESKTOP-SVECMJF\BC <scorbitt@comcast.net>
2021-05-19 14:07:19 -0600

    added line

--- HelloAlbuquerque/Program.cs
+++ HelloAlbuquerque/Program.cs
@@ -7,6 +7,7 @@ namespace HelloAlbuquerque
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Albuquerque!");
+            Console.WriteLine("This is a test!");
         }
     }
 }
