10ca720374550f2db46e41e3d595624480e0dcf6
DESKTOP-SVECMJF\BC <scorbitt@comcast.net>
2021-05-19 13:43:25 -0600

    Add project files.

---
+++ HelloAlbuquerque/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloAlbuquerque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Albuquerque!");
+        }
+    }
+}
