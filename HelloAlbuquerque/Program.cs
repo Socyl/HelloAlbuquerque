879233935e95f228c90ebc2b88ef7d0c80e987df
DESKTOP-SVECMJF\BC <scorbitt@comcast.net>
2021-05-24 14:37:53 -0600

    playing still

--- HelloAlbuquerque/Program.cs
+++ HelloAlbuquerque/Program.cs
@@ -6,7 +6,28 @@ namespace HelloAlbuquerque
     {
         static void Main(string[] args)
         {
+          
             Console.WriteLine("Hello There Albuquerque!");
+            
+
+            string[] languages = new string[]{
+                "C#", "COBOL", "Java",
+                "C++", "Visual Basic", "Pascal",
+                "Fortran", "Lisp", "J#"};
+
+            foreach(string item in languages)
+            {
+                Console.WriteLine(item);  
+            }
+
+            Console.WriteLine();
+            Array.Sort(languages);
+
+            foreach (string item in languages)
+            {
+                Console.WriteLine(item);
+            }
+
         }
     }
 }
